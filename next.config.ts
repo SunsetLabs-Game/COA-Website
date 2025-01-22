7bb216d2cbbe8c5b6c1038b99d2012a49d612d2d
Dario Zamora Rojas <dario@grupomundial.cr>
2025-01-21 18:17:40 -0600

    fix: erase innecesary comments

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
     async redirects() {
         return [
             {
-                source: "/", // Ruta ra z
-                destination: "/home", // Ruta a la que quieres redirigir
-                permanent: true, // Cambiar a false si la redirecci n no es definitiva
+                source: "/", 
+                destination: "/home", 
+                permanent: true, 
             },
         ];
     },
