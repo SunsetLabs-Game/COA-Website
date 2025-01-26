acc93c87a93fcce44dd095cf0c56561d47e58b7a
jimenezz22 <ljimenezbogantes@gmail.com>
2025-01-26 12:57:30 -0600

    refactor: remove unimplemented MetaMask connection handler from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ import MetaMaskIcon from "../components/MetaMaskIcon"; // Importa el componente
 
 export default function Home() {
 
-  function handleConnectMetaMask(): void {
-    throw new Error("Function not implemented.");
-  }
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* Botón de MetaMask en la esquina superior derecha */}
