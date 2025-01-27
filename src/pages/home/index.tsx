ba938e494a124989cd68ad071f2238a6fb68f826
Dario Zamora Rojas <dario@grupomundial.cr>
2025-01-27 17:24:54 -0600

    feat:remove-comments

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -18,19 +18,17 @@ export default function Home() {
       style={{ backgroundImage: "url('/assets/backgroundImage.png')" }}
     >
 <header className="w-full flex justify-between items-center p-6 relative h-[200px]">
-  {/* Logo Citizen */}
+
   <img
     src="/icons/citizen.png"
     alt="Logo"
     className="w-[300px] h-[150px] object-contain absolute top-6 left-6 md:w-[200px] md:h-[100px] sm:w-[120px] sm:h-[60px] sm:top-4 sm:left-4"
   />
-  
-  {/* Navigation Menu */}
+
   <nav className="hidden lg:flex items-center justify-center w-full">
     <NavMenu items={navItems} />
   </nav>
   
-  {/* Profile Image */}
   <img
     src="/icons/myProfile.png"
     alt="Profile"
@@ -50,7 +48,6 @@ export default function Home() {
             alt="Welcome"
             className="max-w-[40rem] mb-16 w-full h-auto md:max-w-[25rem]"
           />
-          {/* Ajuste en los botones para dispositivos móviles */}
           <div className="flex flex-col gap-6 w-full md:flex-row md:justify-center md:gap-[30rem]">
             <button className="bg-[#2C2C2C] text-[#0DABFF] px-8 py-3 rounded-full cursor-pointer transition-all duration-300 ease-out text-base uppercase hover:opacity-80 w-full md:w-auto">
               COMMON MARKET
