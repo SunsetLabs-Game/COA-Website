13c0fc93bbd03490e9594a6d868406a99ed8b900
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-03-27 02:54:14 -0600

    feat: add features page

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,13 +1,18 @@
 import FeaturesCompanions from "@/components/features/features-companions";
+import FeaturesFactions from "@/components/features/features-factions";
 import FeaturesResurrection from "@/components/features/features-resurrection";
+import FeaturesHero from "@/components/features/features-hero";
 
-export default function Home() {
+export default function FeaturesPage() {
   return (
-    <main className="min-h-screen text-white">
+    <main className="min-h-screen bg-[#0e1a2b]">
+      <FeaturesHero />
       <div className="container mx-auto px-4">
+        <FeaturesFactions />
         <FeaturesResurrection />
         <FeaturesCompanions />
       </div>
     </main>
   );
 }
+
